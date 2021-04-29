97185a855107a606ac9b500a7ee5bc01dc8093f2
wmeskine <meskine.wassif@gmail.com>
2021-04-29 12:10:09 +0000

    definition of valid functional interfaces

---
+++ chapter2/src/main/java/me/wassif/functionalprogramming/Project.java
@@ -0,0 +1,29 @@
+package me.wassif.functionalprogramming;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+@FunctionalInterface
+public interface Project {
+	public int getStatus();
+}
+
+// valid definitions of functional interface
+@FunctionalInterface
+interface chunk extends Project {
+}
+
+@FunctionalInterface
+interface Sprint extends Project {
+	public int getStatus();
+}
+
+@FunctionalInterface
+interface Phase extends Project {
+	default int getId() throws NoSuchAlgorithmException {
+		return SecureRandom.getInstanceStrong().hashCode();
+	}
+
+	static void skip(int phase) {
+	}
+}
