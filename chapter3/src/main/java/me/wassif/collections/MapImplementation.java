0b68264d90737243caf01217e7796e56bc238fb8
wmeskine <meskine.wassif@gmail.com>
2021-05-08 12:05:22 +0000

    create collections (6)

--- chapter3/src/main/java/me/wassif/collections/MapImplementation.java
+++ chapter3/src/main/java/me/wassif/collections/MapImplementation.java
@@ -7,16 +7,38 @@ import java.util.TreeMap;
 public class MapImplementation {
 	public static void main(String[] args) {
 		// not sorted
-		Map<Integer, String> hashMap = new HashMap<>();
-		hashMap.put(0, "one");
-		hashMap.put(1, "two");
-		hashMap.put(4, "five");
+		// O(1) search O(1) insert
+		Map<Keyword, String[]> hashMap = new HashMap<>();
+		hashMap.put(new Keyword("int"), new String[] { "int", "INT" });
+		hashMap.put(new Keyword("long"), new String[] { "int", "LONG" });
+		hashMap.put(new Keyword("short"), new String[] { "int", "SHORT" });
 		System.out.println(hashMap);
 		// sorted
+		// O(log) search O(log) insert
 		Map<Integer, String> treeMap = new TreeMap<>();
 		treeMap.put(0, "one");
 		treeMap.put(1, "two");
 		treeMap.put(4, "five");
 		System.out.println(treeMap);
+	}
+}
+
+class Keyword {
+	String value;
+
+	public Keyword(String keyString) {
+		this.value = keyString;
+	}
+
+	@Override
+	public int hashCode() {
+		int prime = 101;
+		int result = 1;
+		return prime * result + ((value == null) ? 0 : value.hashCode());
+	}
+
+	@Override
+	public String toString() {
+		return value + " hash : "+ String.valueOf(hashCode());
 	}
 }
