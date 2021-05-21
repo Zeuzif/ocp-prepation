3aea332b36736c28b4cd22dbf3788a4c7b3900a2
wmeskine <meskine.wassif@gmail.com>
2021-05-21 14:49:00 +0000

    add streams (9)

--- chapter4/src/main/java/me/wassif/streams/PrimitiveStream.java
+++ chapter4/src/main/java/me/wassif/streams/PrimitiveStream.java
@@ -1,10 +1,23 @@
 package me.wassif.streams;
 
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class PrimitiveStream {
 	public static void main(String[] args) {
 		DoubleStream fractions = DoubleStream.iterate(.5, (x) -> x / 2);
 		fractions.limit(6).forEach(System.out::println);
+		// map between types of streams
+		Stream<String> words = Stream.of("a", "ab", "abc");
+		IntStream lenghts = words.mapToInt((str) -> str.length());
+		lenghts.mapToObj(intos -> intos + "").collect(Collectors.joining(" "));
+		// average
+		IntStream marks = IntStream.range(2, 15);
+		OptionalDouble result = marks.average();
+		double res = result.orElseGet(() -> Double.NaN);
+		System.out.println(res);
 	}
 }
